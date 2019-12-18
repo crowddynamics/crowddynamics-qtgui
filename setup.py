eee2230baa7067b0c894ba2697ad3de0776f4f36
Jaan Tollander de Balsch <jaan.tollander@gmail.com>
2019-12-18 13:36:17 +0200

    Readme, license

--- setup.py
+++ setup.py
@@ -13,11 +13,11 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description='',
-    long_description=readfile('README.rst'),
+    long_description=readfile('README.md'),
     author='Jaan Tollander de Balsch',
     author_email='de.tollander@aalto.fi',
     url='https://github.com/jaantollander/crowddynamics-qtgui',
-    license=readfile('LICENSE.txt'),
+    license=readfile('LICENSE'),
     packages=find_packages(),
     entry_points={
         'console_scripts': [
@@ -34,9 +34,6 @@ setup(
         'License :: OSI Approved :: MIT'
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
     # test_suite='',
